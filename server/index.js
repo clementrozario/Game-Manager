8d4aa0ab2e7864559272f15a71746b72bb054851
Clement Delphin <clementcla@outlook.com>
2025-06-05 19:14:27 +0530

    updating game end point

--- server/index.js
+++ server/index.js
@@ -29,6 +29,12 @@ app.post("/api/games",(req,res)=>{
     res.status(201).json(newGame);
 })
 
+app.put("/api/games",(req,res)=>{
+    const newGame = req.body;
+    games.push({...newGame,id:games.length+1});
+    res.status(201).json(newGame);
+})
+
 app.listen(PORT,()=>{
     console.log(`server is listening to port:${PORT}`);
 })
