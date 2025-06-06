7c15e271c641ab3231294461704a638d7cd3be59
Clement Delphin <clementcla@outlook.com>
2025-06-06 19:38:13 +0530

    creating theme manager (dark & light modes)

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,26 @@
 import { Link } from "react-router-dom";
+import { useThemeStore } from "../store/useThemeStore";
 
 function Navbar(){
+    const {theme, toggleTheme} = useThemeStore();
+
+    const isDarkMode = theme === "dark"
+
     return(
         <div className="navbar">
             <div className="navbar-title">My Game Site</div>
             <div className="navbar-links">
                 <Link to="/">Home</Link>
                 <Link to="/create">Add game</Link>
+
+                <label className="theme-switch">
+                    <input type="checkbox" 
+                    onChange={toggleTheme} 
+                    checked={isDarkMode} 
+                    aria-label="Toggle-theme"
+                    />
+                    <span className="slider round"></span>
+                </label>
             </div>
 
         </div>
